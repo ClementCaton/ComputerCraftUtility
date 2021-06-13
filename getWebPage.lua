3c25773c034b10dc51a1f1ba1f8d5fb6bbb60855
ClementCaton <Clement.Caton@algosup.com>
2021-06-13 22:26:12 +0200

    http server

--- getWebPage.lua
+++ getWebPage.lua
@@ -1,1 +1,7 @@
---I need to setup a rasp webpage first
+for i = 1, 1000, 1 do
+    Monitor = peripheral.wrap("right")
+    Data = http.get("http://78.112.232.26/data.txt")
+    Monitor.clear()
+    Monitor.setCursorPos(1,1)
+    Monitor.write(Data.readAll())
+end
