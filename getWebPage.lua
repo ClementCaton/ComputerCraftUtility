0d54a88bd7d4877c48cbc11f0ece0e4754249dec
ClementCaton <clement.caton@algosup.com>
2023-01-04 10:46:34 +0100

    deleted

--- getWebPage.lua
+++
@@ -1,7 +0,0 @@
-while true do
-    Monitor = peripheral.wrap("right")
-    Data = http.get("http://78.112.232.26/data.txt")
-    Monitor.clear()
-    Monitor.setCursorPos(1,1)
-    Monitor.write(Data.readAll())
-end
