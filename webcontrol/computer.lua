b7880da57a8b4c7d15f85b02523fd8502b0fcd9c
ClementCaton <clement.caton@algosup.com>
2023-01-04 21:55:11 +0100

    testcomputer

--- webcontrol/computer.lua
+++ webcontrol/computer.lua
@@ -1,9 +1,14 @@
 modem = peripheral.wrap("left")
 modem.open(1)
 
+screen = peripheral.wrap("right")
+screen.setTextScale(0.5)
+
 local instruction = ""
 
-function Retreive()
-    instruction = http.get("http://84.7.122.8:8080/instruction").readAll()
-    
+while true do
+    local event, modemSide, senderChannel, replyChannel, message, senderDistance = os.pullEvent("modem_message")
+    screen.setCursorPos(1,12)
+    screen.write(message)
+    screen.scroll(1)
 end
