9954a087defa4b65ab7f8a55582a96d72421e39a
ClementCaton <clement.caton@algosup.com>
2023-01-04 11:04:38 +0100

    i forgor to delete you

--- updateFile.php
+++
@@ -1,9 +0,0 @@
-<?php
-$data_to_write = $_POST['DATA'];
-$file_path = "./data.txt";
-
-$file_handle = fopen($file_path, 'w');
-fwrite($file_handle, $data_to_write);
-fclose($file_handle);
-header('Location: index.html');
-?>
