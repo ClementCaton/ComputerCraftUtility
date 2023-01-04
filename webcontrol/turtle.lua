c817a8e34e9f2d254b7c69267956a0c67216b679
ClementCaton <clement.caton@algosup.com>
2023-01-04 22:36:44 +0100

    E

--- webcontrol/turtle.lua
+++ webcontrol/turtle.lua
@@ -3,3 +3,8 @@ local Id = os.getComputerID()
 modem.open(Id)
 
 modem.transmit(1, Id, "Turtle "..Id.." online")
+modem.transmit(1, Id, "Turtle "..Id.." has "..turtle.getFuelLevel().." fuel")
+modem.transmit(1, Id, "Turtle "..Id.." is ready to receive instructions")
+while true do
+
+end
